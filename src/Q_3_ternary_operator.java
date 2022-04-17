8f620334e0dc0de1659295244eb3a32edb6cc1a9
Basan <basantaad817@gmail.com>
2022-04-17 06:33:18 +0545

    question no 3

---
+++ src/Q_3_ternary_operator.java
@@ -0,0 +1,11 @@
+//. Write ternary operator for the question no. 1.
+//variable = Expression ? expression1 : expression2
+public class Q_3_ternary_operator {
+    public static void main(String[] args) {
+        int age = 23;
+        String vote = (age>18)? "yes you can vote":"no, you can't vote";
+
+        System.out.println(vote);
+
+    }
+}
